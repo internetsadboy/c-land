63b78b675f07a558497c98298df90a391176f622
Jared Halpert <jaredhalpert90@gmail.com>
2014-08-27 20:32:11 -0700

    added: putstring, putreverse, putreverseit, stringlen

---
+++ putstring/main.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+
+
+void putstring (char *s)
+{
+  while ('\0' != *s)
+  {
+    printf("\n %d ", s);
+    putchar(*s);
+    s++;
+  }
+}
+
+
+// recursive implementation
+void putreverse (char *s)
+{
+  if(*s != '\0') {
+    putreverse(s + 1);
+  }
+
+  putchar(*s);
+}
+
+
+// iterative implementation
+void putreverseit (char *s)
+{
+  int l = stringlen(s);
+
+  while(l >= 0)
+  {
+    putchar(*(s + l));
+    l--;
+  }
+}
+
+
+int stringlen (char *s)
+{
+	char *i;
+	i = s;
+
+	while ('\0' != *s)
+	{
+		s++;
+	}
+
+  return (int) s - (int) i;
+}
+
+
+int main (int argc, char *argv[])
+{
+  char str[18] = "hello cruel world";
+
+  putstring(str);
+  putchar('\n');
+
+  putreverse(str);
+  putchar('\n');
+
+  printf("[%d]\n", stringlen(str));
+
+  putreverseit(str);
+  putchar('\n');
+
+  return 0;
+}
